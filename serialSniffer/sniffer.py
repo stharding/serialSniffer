1818e7c542b577c5977666fe72684905bbabd605
Stephen Harding <harding@spawar.navy.mil>
2017-01-09 13:52:22 -0800

    storring the bytes

--- serialSniffer/sniffer.py
+++ serialSniffer/sniffer.py
@@ -12,6 +12,8 @@ class Sniffer(object):
         self.virtual_comm = Serial(virtual_comm)
         self.physical_comm = Serial(physical_comm)
         self.pool = ThreadPoolExecutor(4)
+        self.v_bytes = []
+        self.p_bytes = []
 
         self.pool.submit(self.read_physical)
         self.pool.submit(self.read_virtual)
@@ -25,6 +27,7 @@ class Sniffer(object):
         while True:
             p_byte = self.virtual_comm.read()
             self.virtual_comm.write(p_byte)
+            self.p_bytes.append(p_byte)
             # TOCO: store the data somewhere
 
     def read_virtual(self):
@@ -36,3 +39,4 @@ class Sniffer(object):
         while True:
             v_byte = self.virtual_comm.read()
             self.physical_comm.write(v_byte)
+            self.v_bytes.append(v_byte)
